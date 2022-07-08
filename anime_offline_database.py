b02a4641ac771f49d8f5f633b943f404f42e530f
zerolivesleft <51168812+zerolivesleft@users.noreply.github.com>
2022-07-08 00:22:53 -0500

    gitignore

--- anime_offline_database.py
+++ anime_offline_database.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from turtle import done
 
 url = "https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database.json"
 response = requests.get(url)
